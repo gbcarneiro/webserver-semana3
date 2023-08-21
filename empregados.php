7777cbae6d2268d4c5cc4055519c43797bd654d7
Gabriel Carneiro <gabriel.carneiro@sou.inteli.edu.br>
2023-08-20 23:48:06 -0300

    fix comments

--- empregados.php
+++ empregados.php
@@ -5,29 +5,29 @@
     <h1>Empregados</h1>
     <?php
 
-    /* Connect to MySQL and select the database. */
+    /* Conectar ao MySQL e selecionar o banco de dados. */
     $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
     if (mysqli_connect_errno())
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        echo "Falha ao conectar ao MySQL: " . mysqli_connect_error();
 
     $database = mysqli_select_db($connection, DB_DATABASE);
 
-    /* Ensure that the EMPLOYEES table exists. */
-    VerifyEmployeesTable($connection, DB_DATABASE);
+    /* Garantir que a tabela EMPREGADOS exista. */
+    VerificarTabelaEmpregados($connection, DB_DATABASE);
 
-    /* If input fields are populated, add a row to the EMPLOYEES table. */
+    /* Se os campos de entrada estiverem preenchidos, adicionar uma linha à tabela EMPREGADOS. */
     $employee_name = htmlentities($_POST['NAME']);
     $employee_address = htmlentities($_POST['ADDRESS']);
     $employee_birthday = htmlentities($_POST['BIRTHDAY']);
     $employee_salary = htmlentities($_POST['SALARY']);
 
     if (strlen($employee_name) || strlen($employee_address) || strlen($employee_birthday) || strlen($employee_salary)) {
-        AddEmployee($connection, $employee_name, $employee_address, $employee_birthday, $employee_salary);
+        AdicionarEmpregado($connection, $employee_name, $employee_address, $employee_birthday, $employee_salary);
     }
     ?>
 
-    <!-- Input form -->
+    <!-- Formulário de entrada -->
     <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
         <table border="0">
             <tr>
@@ -50,13 +50,13 @@
                     <input type="number" name="SALARY" />
                 </td>
                 <td>
-                    <input type="submit" value="Add Data" />
+                    <input type="submit" value="Adicionar Dados" />
                 </td>
             </tr>
         </table>
     </form>
 
-    <!-- Display table data. -->
+    <!-- Exibir dados da tabela. -->
     <table border="1" cellpadding="2" cellspacing="2">
         <tr>
             <td>ID</td>
@@ -83,7 +83,7 @@
 
     </table>
 
-    <!-- Clean up. -->
+    <!-- Limpeza. -->
     <?php
 
     mysqli_free_result($result);
@@ -98,49 +98,49 @@
 
 <?php
 
-/* Add an employee to the table. */
-function AddEmployee($connection, $name, $address, $birthday, $salary)
+/* Adicionar um empregado à tabela. */
+function AdicionarEmpregado($connection, $nome, $endereco, $aniversario, $salario)
 {
-    $n = mysqli_real_escape_string($connection, $name);
-    $a = mysqli_real_escape_string($connection, $address);
-    $b = mysqli_real_escape_string($connection, $birthday);
-    $s = mysqli_real_escape_string($connection, $salary);
+    $n = mysqli_real_escape_string($connection, $nome);
+    $a = mysqli_real_escape_string($connection, $endereco);
+    $b = mysqli_real_escape_string($connection, $aniversario);
+    $s = mysqli_real_escape_string($connection, $salario);
 
     $query = "INSERT INTO EMPREGADOS (NAME, ADDRESS, BIRTHDAY, SALARY) VALUES ('$n', '$a', '$b', '$s');";
 
     if (!mysqli_query($connection, $query))
-        echo ("<p>Error adding employee data.</p>");
+        echo ("<p>Erro ao adicionar dados do empregado.</p>");
 }
 
-/* Check whether the table exists and, if not, create it. */
-function VerifyEmployeesTable($connection, $dbName)
+/* Verificar se a tabela existe e, caso contrário, criá-la. */
+function VerificarTabelaEmpregados($connection, $dbName)
 {
-    if (!TableExists("EMPREGADOS", $connection, $dbName)) {
+    if (!TabelaExiste("EMPREGADOS", $connection, $dbName)) {
         $query = "CREATE TABLE EMPREGADOS (
          ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
          NAME VARCHAR(45),
          ADDRESS VARCHAR(90),
-	BIRTHDAY DATE, 
-	SALARY DECIMAL(5,2)
+	 BIRTHDAY DATE, 
+	 SALARY DECIMAL(5,2)
        )";
 
         if (!mysqli_query($connection, $query))
-            echo ("<p>Error creating table.</p>");
+            echo ("<p>Erro ao criar tabela.</p>");
     }
 }
 
-/* Check for the existence of a table. */
-function TableExists($tableName, $connection, $dbName)
+/* Verificar a existência de uma tabela. */
+function TabelaExiste($nomeTabela, $connection, $dbName)
 {
-    $t = mysqli_real_escape_string($connection, $tableName);
+    $t = mysqli_real_escape_string($connection, $nomeTabela);
     $d = mysqli_real_escape_string($connection, $dbName);
 
-    $checktable = mysqli_query(
+    $verificarTabela = mysqli_query(
         $connection,
         "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'"
     );
 
-    if (mysqli_num_rows($checktable) > 0)
+    if (mysqli_num_rows($verificarTabela) > 0)
         return true;
 
     return false;
